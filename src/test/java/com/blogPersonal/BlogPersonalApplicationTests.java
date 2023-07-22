77f2fe3cb531a7c7f12ce2b1e8a30717035912d3
Fabian <fabiam0226@gmail.com>
2023-07-21 19:09:45 -0500

    Commit inicial

---
+++ src/test/java/com/blogPersonal/BlogPersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogPersonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
