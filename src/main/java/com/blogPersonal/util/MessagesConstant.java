d0c9c9d39be755e83aec0c3336c29de6b6f088cd
Fabian <fabiam0226@gmail.com>
2023-07-23 21:42:47 -0500

    Implemetacion Util MessageConstant

---
+++ src/main/java/com/blogPersonal/util/MessagesConstant.java
@@ -0,0 +1,39 @@
+package com.blogPersonal.util;
+
+public class MessagesConstant {
+
+    public MessagesConstant() {
+    }
+
+    // Mensajes de éxito
+    public static final int SUCCESS_CODE = 0;
+    public static final String SUCCESS_MESSAGE = "Operación exitosa";
+
+    // Mensajes de validación de campos
+    public static final int ERROR_FIELD_VALIDATION_CODE = 1;
+    public static final String MANDATORY_FIELD = "El campo %s es obligatorio";
+
+    // Mensajes de entidad
+    public static final int ENTITY_ALREADY_EXISTS_CODE = 2;
+    public static final String ENTITY_ALREADY_EXISTS = "Ya existe un %s con ese id";
+
+    public static final int ENTITY_NOT_EXISTS_CODE = 3;
+    public static final String ENTITY_NOT_EXISTS = "No existe un %s con ese id";
+
+    // Mensajes de error genérico
+    public static final int ERROR_DEFAULT_CODE = 99;
+    public static final String ERROR_DEFAULT_MESSAGE = "Error realizando la operación";
+    public static final String ERROR_CUSTOM_MESSAGE = "Error: %s";
+
+    // Mensajes de proceso
+    public static final int PROCESS_ALREADY_EXISTS_CODE = 4;
+    public static final String PROCESS_ALREADY_EXISTS = "El proyecto ya tiene ese proceso";
+
+    // Mensajes de permisos
+    public static final int INSUFFICIENT_PERMISSIONS_CODE = 5;
+    public static final String INSUFFICIENT_PERMISSIONS = "No tienes permisos suficientes para realizar esta acción";
+
+    // Mensajes de conexión
+    public static final int CONNECTION_ERROR_CODE = 6;
+    public static final String CONNECTION_ERROR = "Error de conexión con el servidor";
+}
